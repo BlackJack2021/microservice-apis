49e3545897c20ed7b75c6ec19e26551e583741f0
blackjack <kurawata414@gmail.com>
2024-01-09 17:31:10 +0900

    :sparkles: 6.2 まで実装

--- part2/ch6/orders/api/api.py
+++ part2/ch6/orders/api/api.py
@@ -1,6 +1,7 @@
 import uuid
 from datetime import datetime
 from uuid import UUID
+from typing import Optional
 
 from fastapi import HTTPException
 from starlette.responses import Response
@@ -31,10 +32,35 @@ orders = {
 }
 
 @app.get('/orders', response_model=GetOrdersSchema)
-def get_orders():
-    return {
-        'orders': ORDERS
-    }
+def get_orders(
+    cancelled: Optional[bool] = None,
+    limit: Optional[int] = None
+):
+    # パラメータが設定されていない場合はそのまま返す
+    if cancelled is None and limit is None:
+        return {
+            'orders': ORDERS
+        }
+        
+    # いずれかのパラメータが設定されている場合、絞り込みを実施
+    query_set = [order for order in ORDERS]
+    
+    # cancllled が指定されている場合、指定された条件のもののみ抽出
+    if cancelled is not None:
+        if cancelled:
+            query_set = [
+                order for order in query_set if order['status'] == 'cancelled'
+            ]
+        else:
+            query_set = [
+                order for order in query_set if order['status'] != 'cancelled'
+            ]
+    
+    # limit が指定されている場合、query_set よりその値が小さい場合、数を減らす
+    if limit is not None and len(query_set) > limit:
+        return {'orders': query_set[:limit]}
+    
+    return {'orders': query_set}
 
 @app.post('/orders', status_code=status.HTTP_201_CREATED)
 def create_order(
