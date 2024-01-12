1f9f9c811221525a9fe79e782a09c5400133dc2e
blackjack <kurawata414@gmail.com>
2024-01-12 22:04:07 +0900

    :sparkles: ch6まで完了

--- part2/ch6/kitchen/app.py
+++ part2/ch6/kitchen/app.py
@@ -3,6 +3,10 @@ from flask_smorest import Api
 from .config import BaseConfig
 from .api.api import blueprint
 
+from pathlib import Path
+import yaml
+from apispec import APISpec
+
 # Flask アプリケーションオブジェクトのインスタンスを作成
 app = Flask(__name__)
 
@@ -15,3 +19,12 @@ kitchen_api = Api(app)
 # Blueprint を厨房APIオブジェクトに登録
 kitchen_api.register_blueprint(blueprint)
 
+oas_file = (Path(__file__).parent / "oas.yaml").read_text()
+api_spec = yaml.safe_load(oas_file)
+spec = APISpec(
+    title=api_spec['info']['title'],
+    version=api_spec['info']['version'],
+    openapi_version=api_spec['openapi']
+)
+spec.to_dict = lambda: api_spec
+kitchen_api.spec = spec
