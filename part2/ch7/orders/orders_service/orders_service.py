2fc55b083948c0049b7601bf92bb60611bce3882
blackjack <kurawata414@gmail.com>
2024-01-22 15:46:51 +0900

    ✨ APIが利用する統一モジュールを完成

--- part2/ch7/orders/orders_service/orders_service.py
+++ part2/ch7/orders/orders_service/orders_service.py
@@ -1,12 +1,68 @@
-# 以下の実装は良くない例。何故なら、注文リポジトリのインポートとインスタンス化を注文サービスが引き受けてしまっており、責任が重たい。
-#from repository.orders_repository import OrdersRepository
-#class OrdersService:
-#    def __init__(self):
-#        self.repository = OrdersRepository()
-
-# 以下の「依存性の注入」を利用したパターンでは、
-# 注文リポジトリのインスタンス化と設定を正しく行う責任を呼び出し元にゆだねられる。
 from repository.interface import OrderRepositoryInterface
+from exceptions import OrderNotFoundError
+
+from typing import List
+from orders.types import Item, OrderId
+
 class OrdersService:
     def __init__(self, orders_repository: OrderRepositoryInterface):
         self.orders_repository = orders_repository
+    
+    def place_order(self, items: List[Item]):
+        '''データベースレコードを作成して注文を実行'''
+        return self.orders_repository.add(items)
+    
+    def get_order(self, order_id: OrderId):
+        '''注文リポジトリにリクエストされたIDを渡して注文の詳細を取得'''
+        order = self.orders_repository.get(order_id)
+        if order is not None:
+            return order
+        raise OrderNotFoundError(
+            f'Order with id {order_id} not found'
+        )
+        
+    def update_order(self, order_id: OrderId, items: List[Item]):
+        '''指定されたIDの注文を更新'''
+        order = self.orders_repository.get(order_id)
+        if order is None:
+            raise OrderNotFoundError(
+                f'Order with id {order_id} not found.'
+            )
+        return self.orders_repository.update(
+            order_id,
+            {'items': items}
+        )
+    
+    def list_orders(self, **filters):
+        '''注文をリスト化して受け取り'''
+        limit = filters.pop('limit', None)
+        return self.orders_repository.list(limit, **filters)
+        
+    
+    def pay_order(self, order_id: OrderId):
+        '''指定されたIDの注文に対する支払い'''
+        order = self.orders_repository.get(order_id)
+        if order is None:
+            raise OrderNotFoundError(
+                f'Order with id {order_id} not found'
+            )
+        order.pay()
+        # 注文をスケジュールした後、 schedule_id を更新
+        schedule_id = order.schedule()
+        return self.orders_repository.update(
+            order_id,
+            status='progress',
+            schedule_id=schedule_id
+        )
+
+    def cancel_order(self, order_id: OrderId):
+        order = self.orders_repository.get(order_id)
+        if order is None:
+            raise OrderNotFoundError(
+                f'Order with id {order_id} not found.'
+            )
+        order.cancel()
+        return self.orders_repository.update(
+            order_id,
+            status="cancelled"
+        )
